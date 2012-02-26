8a9dcfb7aff59d011354ab9a1aacd7e00db3a5ca
Dion Amago <dion@transition9.com>
2012-02-25 19:35:42 -0800

    You can't create Sprites or MovieClips until Spaceport is ready.

--- src/spaceport/Lib.hx
+++ src/spaceport/Lib.hx
@@ -2,6 +2,12 @@ package spaceport;
 
 @:native("haxe.Spaceport")
 extern class Lib {
+	/**
+	  * This is null until spaceport is initialized
+	  */
 	public static var current :flash.display.MovieClip;
+	/**
+	  * Notifies callbacks when Spaceport is ready. 
+	  */
 	public static function onSpaceportReady (cb :Void->Void) :Void;
 }
